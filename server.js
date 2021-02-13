2a2d94978b0e1f85628e84da4753dc8fd0ce6074
Sloan Finger <42580194+sloanfinger@users.noreply.github.com>
2021-02-12 21:50:38 -0500

    Missing )

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.use('/assets', express.static(path.join(__dirname + '/assets')));
-app.get('/favicon.ico', express.static(path.join(__dirname, '/assets/images/favicon.ico'));
+app.get('/favicon.ico', express.static(path.join(__dirname, '/assets/images/favicon.ico')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/views/index.html'));
