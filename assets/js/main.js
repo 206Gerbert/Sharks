ac66b33b55f8bf89e1d7a5520107017fc726135b
Sloan Finger <42580194+sloanfinger@users.noreply.github.com>
2021-03-23 22:02:03 -0400

    Under Construction

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,24 +4,34 @@ $(document).ready(() => {
 
     if ($page.length) {
         $('main > section[id="/404"]').remove();
-        $(document.scrollingElement).scrollTop($page.offset().top);
+        $(document.scrollingElement).scrollTop($page.offset().top - $('.navbar').outerHeight());
     } else {
         $('main > section[id!="/404"]').remove();
-        $(document.scrollingElement).scrollTop($('main > section[id="/404"]').offset().top);
+        $(document.scrollingElement).scrollTop($('main > section[id="/404"]').offset().top - $('.navbar').outerHeight());
     }
 
-    $('a[href]').on('click', function(e) {
+    $('a[href]:not([target=_blank])').on('click', function(e) {
         if ($page.length) e.preventDefault();
         $(this).blur();
         
         const destination = new URL($(this).attr('href'), window.location.origin);
 
         $(document.scrollingElement).animate({ 
-            scrollTop: $(`[id="${destination.pathname}"]`).offset().top 
+            scrollTop: $(`[id="${destination.pathname}"]`).offset().top - $('.navbar').outerHeight() 
         }, 1000, 'easeInOutCubic', () => {
             if (destination.pathname !== window.location.pathname) window.location.href = destination.href;
         });
 
+    });
+
+    $('#activate-form').click(function() {
+        $(this).parent().css('display', 'none');
+        $('#form').css('display', '');
+    });
+
+    $('#form').on('reset', function() {
+        $(this).css('display', 'none');
+        $('#activate-form').parent().css('display', '');
     });
 
 });
